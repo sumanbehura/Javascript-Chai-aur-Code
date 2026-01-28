828ec0e4b0b296284c372c2f7c84c1845aaa5198
sumanbehura <sumanbehura100@gmail.com>
2026-01-28 20:21:16 +0530

    Comparision_operator

---
+++ 01_Basics/04_Comparision_operator.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 1);
+//console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02"> 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//we should avoid these type of conversions because these type of conversions can lead us into confusions
+//rather we should focused more towards writing clean code 
+
+// ===
+console.log("2" === 2); 
