d3408521136d33e1c07f5dc15b3516d2d9af84d1
sumanbehura <sumanbehura100@gmail.com>
2026-02-01 21:13:06 +0530

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,11 +1,51 @@
-let a = 300
+let a = 300;
 if (true) {
-let a = 10
-const b = 20
-// console.log("INNER: ", a);
-
+  let a = 10;
+  const b = 20;
+  // console.log("INNER: ", a);
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c)
+
+
+// ++++++++++++++++++++Scope level and mini hoisting++++++++++++++++++++++
+
+function one() {
+  const username = "hitesh";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website); // This line will cause an error
+  two();
+}
+
+// one()
+
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = "youtube";
+    // console.log(username + website);
+  }
+  // console.log(website); // This line will cause an error
+}
+
+// console.log(username); // This line will cause an error
+
+// +++++++++++++++ interesting +++++++++++++++
+
+console.log(addone(5)); // No error here
+
+function addone(num) {
+  return num + 1;
+}
+
+// addTwo(5) // Error: Cannot access 'addTwo' before initialization
+
+const addTwo = function (num) {
+  return num + 2;
+};
