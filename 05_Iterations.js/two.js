bbdcc6fe800f9672d01fe2e74a973dfee5e70c02
sumanbehura <sumanbehura100@gmail.com>
2026-02-02 23:03:10 +0530

    do while loop

---
+++ 05_Iterations.js/two.js
@@ -0,0 +1,23 @@
+// let index = 0
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index+=2
+// }
+
+
+let myArray = ["Flash","Batman","Superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]} `)
+    arr+=1
+}
+
+
+//do while loop
+let score = 1
+
+do {
+    console.log(`Your score is ${score}`);
+    score++
+} while (score<=10);
